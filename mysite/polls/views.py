0d20f8adf4cf24c9d6db1969064743bd7a0dced6
vatsap <vatsamail@gmail.com>
2018-03-11 20:10:17 -0700

    after completing the tutorial - https://docs.djangoproject.com/en/2.0/intro/tutorial03

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,53 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+# from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
-        return HttpResponse("Hello, world! This is poll/index")
+        #return HttpResponse("Hello, world! This is poll/index")
+        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+        #temp_list = []
+        #for lq in latest_question_list:
+        #    temp_list.append(lq.question_text)
+        #out = ', '.join(temp_list)
+
+        #output = ', '.join([q.question_text for q in latest_question_list])
+        #return HttpResponse(output)
+
+        #template = loader.get_template('polls/index.html')
+        context = {
+            'latest_question_list' : latest_question_list,
+        }
+        #return HttpResponse(template.render(context, request))
+        return render(request, 'polls/index.html', context)
+
+
+
+
+def detail(request, question_id):
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+
+    #try:
+    #    question = Question.objects.get(id=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question doesn't exist")
+    #context = {
+    #    'question': question,
+    #}
+    #render(request, 'polls/detail.html', context)
+
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        'question':question,
+    }
+    return (render(request, 'polls/detail.html', context))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
