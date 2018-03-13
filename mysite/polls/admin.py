b38278b9bf1ecc1177c0dec8b34fa66564750052
vatsap <vatsamail@gmail.com>
2018-03-12 22:21:39 -0700

    https://docs.djangoproject.com/en/2.0/intro/tutorial07/

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+#admin.site.register(Question)
+#admin.site.register(Choice)
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text'] # puts fields in the same order
+    fieldsets = [
+        (None,                {'fields': ['question_text']}),
+        ('Date information',  {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
