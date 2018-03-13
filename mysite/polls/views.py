9ffeb7d2df5d609c660c2d61ce5338030051886e
Vatsa Prahallada <svatsa@qti.qualcomm.com>
2018-03-12 18:14:57 -0700

    After completing the tutorial https://docs.djangoproject.com/en/2.0/intro/tutorial04/

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
 # from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html' # overriding default polls/polls_index.html expectation
+    context_object_name = 'latest_question_list' # overriding default question_list by latest_question_list
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 def index(request):
         #return HttpResponse("Hello, world! This is poll/index")
@@ -16,6 +24,15 @@ def index(request):
         #return HttpResponse(output)
 
         #template = loader.get_template('polls/index.html')
+
+        '''
+        corresponding index.html code:
+
+
+          <li><a href="/polls/{{ question.id }}/">{{ question.question_text }}</a></li>
+          <li><a href="{% url 'detail' question.id %}">{{ question.question_text }}</a></li>
+
+        '''
         context = {
             'latest_question_list' : latest_question_list,
         }
@@ -24,6 +41,9 @@ def index(request):
 
 
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 def detail(request, question_id):
     # return HttpResponse("You're looking at question %s." % question_id)
@@ -38,6 +58,22 @@ def detail(request, question_id):
     #}
     #render(request, 'polls/detail.html', context)
 
+    '''
+     corresponding detail.html code:
+
+     <b>{{ question }}</b>
+
+
+     <h1>{{ question.question_text }}</h1>
+     <ul>
+     {% for choice in question.choice_set.all %}
+         <li>{{ choice.choice_text }}</li>
+     {% endfor %}
+     </ul>
+
+
+    '''
+
     question = get_object_or_404(Question, id=question_id)
     context = {
         'question':question,
@@ -45,9 +81,35 @@ def detail(request, question_id):
     return (render(request, 'polls/detail.html', context))
 
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    #response = "You're looking at the results of question %s."
+    #return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/results.html', context)
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    #return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question':question,
+            'error_message': "You did not select a choice",
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
